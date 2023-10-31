e92ab59b3c1f83f1587140a37468c16390e35ce9
Andrés Ravelo <andres.ravelo@bioquimica.cl>
2023-10-31 16:23:38 -0300

    cleanup: removes unnecessary script

--- config/middleware.py
+++
@@ -1,7 +0,0 @@
-class DomainMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        request.domain = request.META['HTTP_HOST']
-        return self.get_response(request)
