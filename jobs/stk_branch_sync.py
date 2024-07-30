5537587aef059bb29d935839159b47fa48f73a61
Andrés <andres.ravelo@bioquimica.cl>
2024-07-30 10:43:50 -0400

    the import of the service code is corrected

--- jobs/stk_branch_sync.py
+++ jobs/stk_branch_sync.py
@@ -2,7 +2,7 @@ from classes.starken.branch import Branch
 from classes.starken.starken import Starken as Stk
 from module.general.models.service_account import ServiceAccount
 
-serv_accts = ServiceAccount.objects.filter(service__code=Stk.SERV_CODE,
+serv_accts = ServiceAccount.objects.filter(service__code=Stk.serv_code,
                                            enabled=True)
 for serv_acct in serv_accts:
     Branch(account=serv_acct).app_sync()
