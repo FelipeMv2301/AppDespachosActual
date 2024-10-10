be00680895413bed49daafbe9691ff4a57e031d0
Andrés <andres.ravelo@bioquimica.cl>
2024-10-10 10:56:50 -0300

    the API delivery data entry flow is updated.

--- api/views/delivery_form.py
+++ api/views/delivery_form.py
@@ -14,9 +14,20 @@ from api.permissions.edit_deliv_form_from_api import \
     EditDelivFormFromApiPermission
 from helpers.decorator.domain import domain_check
 from helpers.decorator.loggable import loggable
+from module.business_partner.models.bsns_partner import BusinessPartner
+from module.business_partner.models.contact import Contact
+from module.business_partner.models.group import Group
+from module.business_partner.models.type import Type
 from module.delivery.models.opt import Option
+from module.general.models.address import Address
+from module.general.models.currency import Currency
+from module.general.models.employee import Employee
+from module.general.models.muni import Muni
+from module.general.models.service_account import ServiceAccount
 from module.order.models.grouping import Grouping
 from module.order.models.order import Order
+from module.order.models.sale_channel import SaleChannel
+from module.order.models.status import Status
 
 
 class DeliveryFormView(APIView):
@@ -28,6 +39,116 @@ class DeliveryFormView(APIView):
     @domain_check(allowed_domains=allowed_domains)
     @loggable
     def post(self, request: Request, *args, **kwargs):
+        data = request.data
+
+        response_msg = 'OK'
+        response_status = HTTP_202_ACCEPTED
+
+        order_ref = data.get('order_ref')
+        if Order.objects.filter(doc_num=order_ref):
+            return Response(status=response_status)
+
+        # Creación de dirección de facturación
+        bill_muni = Muni.objects.get(code=data.get('bill_muni_code'))
+        bill_addr = Address.objects.create(
+            st_and_num=data.get('bill_street_and_number'),
+            complement=data.get('bill_complement'),
+            muni=bill_muni,
+        )
+
+        # Creación de dirección de entrega
+        ship_muni = Muni.objects.get(code=data.get('ship_muni_code'))
+        ship_addr = Address.objects.create(
+            st_and_num=data.get('ship_street_and_number'),
+            complement=data.get('ship_complement'),
+            muni=ship_muni,
+        )
+
+        # Creación o actualización de cliente
+        tax_id = data.get('customer_tax_id')
+        customer = BusinessPartner.objects.filter(tax_id=tax_id)
+        if customer:
+            customer.update(
+                name=data.get('customer_name'),
+                phone1=data.get('customer_phone1'),
+                phone2=data.get('customer_phone2'),
+                mobile_phone=data.get('customer_mobile_phone'),
+                email_addr=data.get('customer_email_addr'),
+            )
+        else:
+            currency = Currency.objects.get(code='CLP')
+            customer_group = Group.objects.get(code='CN')
+            customer_type = Type.objects.get(code='CL')
+            customer = BusinessPartner.objects.create(
+                code=f'CN{tax_id}',
+                name=data.get('customer_name'),
+                currency=currency,
+                group=customer_group,
+                tax_id=tax_id,
+                type=customer_type,
+                phone1=data.get('customer_phone1'),
+                phone2=data.get('customer_phone2'),
+                mobile_phone=data.get('customer_mobile_phone'),
+                email_addr=data.get('customer_email_addr'),
+            )
+
+        # Creación de contacto
+        contact = Contact.objects.create(
+            first_name=data.get('contact_first_name'),
+            last_name=data.get('contact_last_name'),
+            addr=ship_addr,
+            phone1=data.get('contact_phone1'),
+            phone2=data.get('contact_phone2'),
+            mobile_phone=data.get('contact_mobile_phone'),
+            email_addr=data.get('contact_email_addr'),
+        )
+
+        # Creación del pedido
+        sale_channel = SaleChannel.objects.get(code='WEB')
+        seller = Employee.objects.get(code='PTAN10')
+        status = Status.objects.get(code='CLOSE')
+        service_acct = ServiceAccount.objects.get(code='SAPBQ01')
+        order = Order.objects.create(
+            doc_num=order_ref,
+            reference=order_ref,
+            service_acct=service_acct,
+            currency=currency,
+            web_order_ref=order_ref,
+            sale_channel=sale_channel,
+            seller=seller,
+            create_date=data.get('creation_date'),
+            tax_date=data.get('tax_date'),
+            commit_date=data.get('commit_date'),
+            updtd_commit_date=data.get('commit_date'),
+            ship_addr=ship_addr,
+            bill_addr=bill_addr,
+            customer=customer,
+            contact=contact,
+            local_total_dcnt=data.get('total_discount'),
+            doc_total_dcnt=data.get('total_discount'),
+            local_total_tax=data.get('local_total_tax'),
+            doc_total_tax=data.get('local_total_tax'),
+            local_total_amt=data.get('local_total_amt'),
+            doc_total_amt=data.get('local_total_amt'),
+            status=status,
+        )
+
+        # Creación de la agrupación de pedido
+        deliv_opt = Option.objects.get(id=data.get('delivery_option_id'))
+        Grouping.objects.create(
+            code=Grouping.new_code(),
+            order=order,
+            delivery_option=deliv_opt,
+            addr=ship_addr,
+            customer=customer,
+            contact=contact,
+        )
+
+        return Response(data={'message': response_msg}, status=response_status)
+
+    @domain_check(allowed_domains=allowed_domains)
+    @loggable
+    def obsolete_post(self, request: Request, *args, **kwargs):
         user = request.user
         data = request.data
 
