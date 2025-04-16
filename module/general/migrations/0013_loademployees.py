42f9d9463cf4f2269163ec58f5ea1a16cfd73277
Andrés <andres.ravelo@bioquimica.cl>
2025-04-16 08:20:03 -0400

    se crea migración para cargar nuevo vendedor

---
+++ module/general/migrations/0013_loademployees.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+from django.core.management import call_command
+from django.db import migrations
+
+CURRENT_PATH = Path(__file__).parent.resolve()
+
+
+def forwards_func(apps, schema_editor):
+    CURRENT_PATH = Path(__file__).parent.resolve()
+    data_path = os.path.join(CURRENT_PATH, 'data')
+    data_file_names = [
+        'employee7.json',
+        'employee_service6.json',
+    ]
+    for filename in data_file_names:
+        call_command('loaddata', os.path.join(data_path, filename))
+
+
+def reverse_func(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('general', '0012_loademployees'),
+    ]
+
+    operations = [
+        migrations.RunPython(code=forwards_func, reverse_code=reverse_func),
+    ]
